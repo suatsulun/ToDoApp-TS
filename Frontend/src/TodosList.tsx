06d4ca97969016640078d6a08aa7aa573d3a8b98
suatsulun <suatsulun@protonmail.com>
2026-03-09 00:42:13 +0300

    Day 2

--- Frontend/src/TodosList.tsx
+++ Frontend/src/TodosList.tsx
@@ -2,6 +2,15 @@ import { useState } from 'react'
 import { Input } from './components/ui/input'
 import { Button } from './components/ui/button'
 import { Plus, Trash } from 'lucide-react'
+import { ButtonGroup } from "@/components/ui/button-group"
+import { Field } from "@/components/ui/field"
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 
 type Todo = {
@@ -37,20 +46,30 @@ const TodosList = (): React.JSX.Element =>{
     }
 
     return <div>
-        <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder='Write ToDos here...'/>
-        <Button onClick={handleAdd}><Plus />Add</Button>
-        <Button onClick={handleDeleteAll}><Trash />Delete All</Button>
-        <div>{todos.map((todo:Todo): React.JSX.Element => (
-            <div key={todo.id}>
-            <div>{todo.text}</div>
-            <div>{todo.createdAt}</div>
-            <Button onClick={() => handleDelete(todo.id)}><Trash />Delete</Button>
+        <div className="flex flex-row">
+        <Field >
+            <ButtonGroup >
+            <Input className="border-1 border-black rounded-full" value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Write ToDos here..."/>
+            <Button className="flex-initial border-1 border-black rounded-full" onClick={handleAdd} variant="default"><Plus />Add</Button></ButtonGroup>
+        </Field>
+        <Button className="flex-initial border-1 border-red-600 rounded-full ml-2" variant="destructive" onClick={handleDeleteAll}><Trash />Delete All</Button></div>
+        <div className="flex flex-wrap gap-2 w-full">{todos.map((todo:Todo): React.JSX.Element => (
+            <div key={todo.id} className="inline-block">
+                <Card className="w-fit">
+                <CardHeader>
+                    <CardTitle className='w-max'>{todo.text}</CardTitle>
+                    <CardDescription><span>Created at:</span><br />{todo.createdAt}</CardDescription>
+                </CardHeader>
+                <CardFooter className="flex-column">
+                    <Button className="border-1 border-red-600 rounded-full ml-2 w-full" onClick={() => handleDelete(todo.id)} variant="destructive">
+                        <Trash />Delete
+                    </Button>
+                </CardFooter>
+            </Card>
             </div>
         ))
             }
         </div>
-        
-        
     </div>
 }
 
